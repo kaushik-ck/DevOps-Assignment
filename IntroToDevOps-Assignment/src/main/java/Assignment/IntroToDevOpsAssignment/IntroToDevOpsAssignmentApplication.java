d387367b5a4faf1db65ff434b8335387d2ca6a42
Ashish <ashish.mohanty135@gmail.com>
2024-05-05 13:51:40 +0530

    Fist Commit

---
+++ IntroToDevOps-Assignment/src/main/java/Assignment/IntroToDevOpsAssignment/IntroToDevOpsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package Assignment.IntroToDevOpsAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToDevOpsAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToDevOpsAssignmentApplication.class, args);
+	}
+
+}
---
+++ IntroToDevOps-Assignment/src/test/java/Assignment/IntroToDevOpsAssignment/IntroToDevOpsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package Assignment.IntroToDevOpsAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToDevOpsAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
